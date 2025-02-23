36887845179ba4f37b7d0faf9858ee87fd2f87d5
David Glunchadze <davitiglunchadze@gmail.com>
2025-02-23 17:12:37 +0400

    Add first section ACF

--- front-page.php
+++ front-page.php
@@ -1,3 +1,19 @@
+
+
+<?php 
+$section_1 = get_field('1_section');
+$hero_section_image = $section_1['hero_section_image'];
+$hero_title = $section_1['hero_title'];
+$hero_text = $section_1['hero_text'];
+$hero_button = $section_1['hero_button'];
+$hero_button_link = $section_1['hero_button_link'];
+
+//echo '<pre>';
+//print_r ($section_1);
+//echo '</pre>';
+?>
+
+
 <?php 
 get_header();
 ?>
@@ -6,13 +22,15 @@ get_header();
 <main class="home-page">
     <!--FIRST SECTION-->
     <section class="hero-section-1">
-        <img src=" <?php echo get_template_directory_uri(). "/assets/images/main-photo.png" ?>" alt="main-photo">
+        <?php if(!empty($hero_section_image['url']) && !empty($hero_section_image['alt']) ) :?>
+            <img src=" <?php echo esc_url($hero_section_image['url']) ?>" alt="<?php echo esc_url($hero_section_image['alt'])?>">
+        <?php endif; ?>
         <div class="hero-content">
-            <h1 class="hero-title">Your Imagination Is 
-            Your Only Limit</h1>
-            <p class="hero-text">We always try to make our customer Happy. We provide all kind of facilities. 
-            Your Satisfaction is our main priority</p>
-            <a class="hero-button" href="#">Discover more</a>
+                <h1 class="hero-title"><?php echo $hero_title; ?></h1>
+                <p class="hero-text"><?php echo $hero_text; ?> </p>
+            <?php if($hero_button_link):?>
+                <a class="hero-button" href="<?php echo $hero_button_link; ?>"><?php echo $hero_button; ?></a>
+            <?php endif; ?>
         </div>
     </section>
 </main>
